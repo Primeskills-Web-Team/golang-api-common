e2f0e9c97ceeb37f49373c3396c17d811e82c3dd
Muhammad Suryono <muhammad.suryono@primeskills.id>
2024-01-21 15:11:17 +0700

    add router setup

--- server/server.go
+++ server/server.go
@@ -12,7 +12,10 @@ func Run(port string, resource string) {
 	if port == "" {
 		log.Fatalln("Port is undefined, please setup your port")
 	}
+	RunDefaultServer(port, resource, nil)
+}
 
+func RunDefaultServer(port string, resource string, routes func(e *gin.Engine)) {
 	srv := primeskillsserver.NewPrimeskillsServer()
 	srv.SetException(exception.ErrorHandler)
 	srv.SetStatusMethodNotAllowed(func(c *gin.Context) {
@@ -21,5 +24,9 @@ func Run(port string, resource string) {
 	srv.SetStatusNotFound(func(c *gin.Context) {
 		panic(exception.NewNotFoundErrorError(fmt.Sprintf("Path %s not found", c.Request.RequestURI)))
 	})
+
+	if routes != nil {
+		srv.SetRouters(routes)
+	}
 	srv.RunServer(port, resource)
 }
